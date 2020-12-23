84d0c355010d5778e18efdfaf43b1d2e1c0ec616
Ankit Kalia <ankitkalia.dev@gmail.com>
2020-12-23 14:57:05 +0700

    add function documentation

--- lib/redisearch/index.rb
+++ lib/redisearch/index.rb
@@ -1,11 +1,22 @@
 module RediSearch
   class Index
     class << self
+      # Creates a new index
+      # @param [String] name
+      # @param [String] prefix
+      # @param [Hash] schema
+      # Example Usage:
+      # RediSearch::Index.create(name: 'products', prefix: 'products:', schema: { id: 'NUMERIC SORTABLE', name: 'TEXT SORTABLE' })
       def create(name:, prefix:, schema:)
         command = "FT.CREATE #{name} ON HASH PREFIX 1 #{prefix} SCHEMA #{schema.to_a.flatten.join(' ')}"
         REDIS.call(command.split(' '))
       end
 
+      # Destroys an existing index
+      # @param [String] name
+      # @param [Boolean] destroy_documents
+      # Example Usage:
+      # RediSearch::Index.destroy(name: 'products')
       def destroy(name:, destroy_documents: true)
         command = "FT.DROPINDEX #{name}"
         command << ' DD' if destroy_documents
@@ -13,6 +24,8 @@ module RediSearch
         REDIS.call(command.split(' '))
       end
 
+      # Get info on existing index
+      # @param [String] name
       def info(name:)
         REDIS.call('FT.INFO', name)
       end
